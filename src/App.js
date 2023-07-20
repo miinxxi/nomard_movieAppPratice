1d219a2423e3e4f3daee9ee131fd17f40855d3d6
miinxxi <whgh13@naver.com>
2023-07-20 21:51:24 +0900

    update5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "routes/Detail";
 import Home from "routes/Home";
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`}  element={<Home />}></Route>
         <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />}></Route>
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 export default App;
